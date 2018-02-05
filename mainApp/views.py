b5e694a996b235c37d1f2f8710f9268c924af0f8
Goodron1981 <Ponamarev7@gmail.com>
2018-02-06 01:00:02 +0200

    обновление
    обновление

--- mainApp/views.py
+++ mainApp/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'mainApp/homePage.html')
+
+def contact(request):
+    return render(request, 'mainApp/basic.html', {'values': ['Если у Вас остались вопросы, то задавайте их по телефону', '(068) 066-65-65']})
--- news/urls.py
+++ news/urls.py
@@ -3,5 +3,5 @@ from django.views.generic import ListView, DetailView
 from news.models import Articles
 
 urlpatterns = [
-    url(r'^$', ListView.as_view(quertset = Articles.objects.all().order_by("-date")[:20], template_name = "news/posts.html"))
+    url(r'^$', ListView.as_view(queryset = Articles.objects.all().order_by("-date")[:20], template_name = "news/posts.html"))
 ]
