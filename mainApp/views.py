7c6e7ebaba233f95487012b1544ac48f6eb52e88
Goodron1981 <Ponamarev7@gmail.com>
2018-02-10 00:43:03 +0200

    Последеее обновление
    Псоледнее обновления

--- mainApp/views.py
+++ mainApp/views.py
@@ -4,4 +4,4 @@ def index(request):
     return render(request, 'mainApp/homePage.html')
 
 def contact(request):
-    return render(request, 'mainApp/basic.html', {'values': ['Если у Вас остались вопросы, то задавайте их по телефону', '(068) 066-65-65']})
+    return render(request, 'mainApp/basic.html', {'values': ['Если у Вас остались вопросы, то задавайте их по телефону', '(068) 066-65-65', 'example@ukr.net']})
